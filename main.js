ec54f3d35582924bc96f452a1d2c681a99deb99e
Mikhail Sivolobov <test@qeep.pro>
2018-08-24 16:07:04 +0700

    Исправлен дизайн калькулятора, добавлены новые кнопки. Немного нового функционала

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 'use strict';
 const input = document.getElementById('num-input');
-let waitingNumber = 0;
-let priorityWaitingNumber = 0;
-let currentOperation = null;
-let inputtingNumber = false;
+let num = 0;
+let p = false;
+let lop = null;
 
 const backspaceButton = document.getElementById('backspace-button');
 backspaceButton.onclick = () => {
@@ -12,15 +11,6 @@ backspaceButton.onclick = () => {
     } else {
         input.value = input.value.slice(0, -1);
     }
-};
-
-const enterNumber = (n) => {
-    if (input.value === '0' || !inputtingNumber) {
-        input.value = n;
-    } else {
-        input.value = input.value + n;
-    }
-    inputtingNumber = true;
 };
 
 const numButtons = document.getElementsByClassName('num-button');
@@ -32,26 +22,48 @@ const numButtons = document.getElementsByClassName('num-button');
 
 const dotButton = document.getElementById('dot-button');
 dotButton.onclick = () => {
+    if (p) {
+        num = parseFloat(input.value);
+        input.value = '0.';
+        p = false;
+    }
     if (input.value.indexOf('.') === -1) {
         input.value += '.';
     }
 };
 
-const dispatchOperation = (operation) => {
-    console.log(waitingNumber, currentOperation);
-    const argumentNumber = parseFloat(input.value);
-    inputtingNumber = false;
-
-    if (!currentOperation) {
-        waitingNumber = argumentNumber;
+const enterNumber = (n) => {
+    if (input.value === '0') {
+        input.value = n;
     } else {
-        waitingNumber = processOperation(currentOperation, waitingNumber, argumentNumber);
+        if (p) {
+            num = parseFloat(input.value);
+            input.value = n;
+            p = false;
+        } else {
+            input.value = input.value + n;
+        }
+    }
+};
+
+const dispatchOperation = (operation) => {
+    if (operation === '±') {
+        input.value = parseFloat(input.value) * -1;
+    }
+    console.log(num, lop, p, operation);
+    if (lop) {
+        if (!p) {
+            num = processOperation(lop, num, parseFloat(input.value));
+            input.value = num;
+        }
     }
 
-    input.value = waitingNumber;
-
-    currentOperation = operation;
-    console.log(waitingNumber, currentOperation);
+    p = true;
+    if (operation !== '=') {
+        lop = operation;
+    } else {
+        lop = null;
+    }
 };
 
 const processOperation = (operation, argument1, argument2) => {
@@ -60,12 +72,14 @@ const processOperation = (operation, argument1, argument2) => {
             return argument1 + argument2;
         case '-':
             return argument1 - argument2;
-        case '*':
+        case '×':
             return argument1 * argument2;
-        case '/':
+        case '÷':
             return argument1 / argument2;
+        case '=':
+            return argument1;
     }
-}
+};
 
 const operationButtons = document.getElementsByClassName('operation-button');
 [].forEach.call(operationButtons, (button) => {
@@ -74,6 +88,21 @@ const operationButtons = document.getElementsByClassName('operation-button');
     }
 });
 
-// DOM — Document Object Model
-// BOM — Browser Object Model
+for (let i = 0; i < operationButtons.length; i++) {
+    const button = operationButtons[i];
 
+    button.onclick = () => {
+        dispatchOperation(button.textContent);
+    }
+}
+
+document.getElementById('clear-all-button').onclick = () => {
+    num = 0;
+    p = false;
+    lop = null;
+    input.value = '0';
+};
+
+document.getElementById('clear-current-button').onclick = () => {
+    input.value = '0';
+};
